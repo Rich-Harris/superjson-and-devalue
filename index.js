f710edd7090801460a5b189fe9c48fef1372d2de
Simon Knott <info@simonknott.de>
2021-02-23 09:27:55 +0100

    Update to SuperJSON v1.7.1

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-const c = require('kleur');
-const superjson = require('superjson');
-const devalue = require('devalue');
+const c = require("kleur");
+const superjson = require("superjson").default;
+const devalue = require("devalue");
+
+const nestedObj = {
+  i: { have: { an: "identity" } },
+};
 
 const obj = {
-	date: new Date(),
-	array: [
-		{ foo: 1 },
-		{ bar: 2 },
-		{ baz: 3 }
-	],
-	regex: /the quick brown fox/,
-	number: 42
+  date: new Date(),
+  set: new Set([{ foo: 1 }, { bar: 2 }, { baz: 3 }]),
+  regex: /the quick brown fox/,
+  number: 42,
+  alice: nestedObj,
+  bob: nestedObj,
 };
 
 const superjson_serialized = superjson.stringify(obj);
 const devalue_serialized = devalue(obj);
 
-console.log(`superjson output: ${c.bold().cyan(superjson_serialized.length)} bytes`);
-console.log(`devalue output: ${c.bold().cyan(devalue_serialized.length)} bytes`);
+console.log(
+  `superjson output: ${c.bold().cyan(superjson_serialized.length)} bytes`
+);
+console.log(
+  `devalue output: ${c.bold().cyan(devalue_serialized.length)} bytes`
+);
 
 // const superjson_deserialized = superjson.parse(superjson_serialized);
 // const devalue_deserialized = eval(`(${devalue_serialized})`);
@@ -25,14 +31,16 @@ console.log(`devalue output: ${c.bold().cyan(devalue_serialized.length)} bytes`)
 const iterations = 1e6;
 
 function test(fn, label = fn.toString()) {
-	const start = Date.now();
-	console.log();
-	console.log(c.bold(label));
-	let i = iterations;
-	while (i--) {
-		fn();
-	}
-	console.log(`${iterations} iterations in ${c.bold().cyan(Date.now() - start)}ms`);
+  const start = Date.now();
+  console.log();
+  console.log(c.bold(label));
+  let i = iterations;
+  while (i--) {
+    fn();
+  }
+  console.log(
+    `${iterations} iterations in ${c.bold().cyan(Date.now() - start)}ms`
+  );
 }
 
 // serialization
